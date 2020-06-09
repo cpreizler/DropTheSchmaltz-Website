6db33288fc73dc07cc5e0b7a5b2b092a00322675
cpreizler <50078832+cpreizler@users.noreply.github.com>
2020-06-09 05:01:39 -0400

    footer

---
+++ footer.js
@@ -0,0 +1,12 @@
+Vue.component('site-footer', {
+    template: `<footer class="footer mt-auto py-3" style="background-color: #343a40; position: fixed; Width: 100%; bottom: 0; right:0; left: 0;">
+    <div class="container">
+      <span class="text-muted">
+		Phone: (212)-NOSHMLTZ &emsp;&emsp; Email: Droptheshmaltz@gmail.com
+	  </span>
+    </div>
+  </footer>`
+  });
+
+  var foot = new Vue({
+	el: '#foot'})
