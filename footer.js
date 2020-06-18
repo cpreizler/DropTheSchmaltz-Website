4dac6d3ace749a5b435bfb0ca00d870c5b067dcc
cpreizler <50078832+cpreizler@users.noreply.github.com>
2020-06-18 03:08:09 -0400

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,7 +2,8 @@ Vue.component('site-footer', {
     template: `<footer class="footer mt-auto py-3" style="background-color: #343a40; position: fixed; Width: 100%; bottom: 0; right:0; left: 0;">
     <div class="container">
       <span class="text-muted">
-		Phone: (212)-NOSHMLTZ &emsp;&emsp; Email: Droptheshmaltz@gmail.com
+		Phone: (212)-NOSHMLTZ &emsp;&emsp; Email: Droptheshmaltz@gmail.com  
+Site brought to you by: Chana Preizler and Ezra Koppel
 	  </span>
     </div>
   </footer>`
